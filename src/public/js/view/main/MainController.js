a14c5debfe1e4b9dc607e03cf6e9b3c4a7fd7f7d
Piotr Kosmowski <kospiotr@gmail.com>
2014-06-22 23:30:37 +0200

    Update Openshift configuration

---
+++ src/public/js/view/main/MainController.js
@@ -0,0 +1,24 @@
+/**
+ * This class is the main view for the application. It is specified in app.js as the
+ * "autoCreateViewport" property. That setting automatically applies the "viewport"
+ * plugin to promote that instance of this class to the body element.
+ */
+Ext.define('App.view.main.MainController', {
+    extend: 'Ext.app.ViewController',
+
+    requires: [
+        'Ext.MessageBox'
+    ],
+
+    alias: 'controller.main',
+
+    onClickButton: function () {
+        Ext.Msg.confirm('Confirm', 'Are you sure?', 'onConfirm', this);
+    },
+
+    onConfirm: function (choice) {
+        if (choice === 'yes') {
+            //
+        }
+    }
+});
