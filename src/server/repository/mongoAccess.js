7dd20e9adf323a2e20bf83e3f2465ef3b3f69bbb
Piotr Kosmowski <kospiotr@gmail.com>
2014-10-16 22:24:17 +0200

    Update mongo access

--- src/server/repository/mongoAccess.js
+++ src/server/repository/mongoAccess.js
@@ -2,7 +2,13 @@ var mongoose = require('mongoose');
 
 var MongoAccess = function () {
   mongoose.set('debug', true)
-  this.connection = mongoose.createConnection('mongodb://admin:rmFTG7vgNnjy@localhost:27017/spinkajs'); // connect to our database
+  var host = process.env.OPENSHIFT_MONGODB_DB_HOST || "localhost";
+  var port = process.env.OPENSHIFT_MONGODB_DB_PORT || "27017";
+  var dbName = process.env.OPENSHIFT_APP_NAME || "spinkajs";
+  var userName = process.env.OPENSHIFT_MONGODB_DB_USERNAME || "admin";
+  var password = process.env.OPENSHIFT_MONGODB_DB_PASSWORD || "rmFTG7vgNnjy";
+  
+  this.connection = mongoose.createConnection('mongodb://'+userName+':'+password+'@'+host+':'+port+'/'+dbName);
 
 }
 
