7388d1016ba4fe6f6a1a2059d54ee9b1340763a8
Piotr Kosmowski <kospiotr@gmail.com>
2014-11-19 15:55:19 -0500

    Remove unused classess

--- src/client/app/scientists/ScientistController.js
+++
@@ -1,9 +0,0 @@
-Ext.define('App.scientists.ScientistController', {
-    extend: 'Ext.app.ViewController',
-    alias: 'controller.scientists',
-    control: {
-    },
-    init: function () {
-        Ext.log('Init ScientistController');
-    }
-});
